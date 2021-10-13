d2c5e087914f5a712d1a2cd4d1f3859d172c38cb
Pierre Geyer <pg@next-motion.de>
2021-10-13 18:43:10 +0200

    Add TYPO3 v11 compatibility

--- ext_emconf.php
+++ ext_emconf.php
@@ -4,15 +4,15 @@ $EM_CONF[$_EXTKEY] = [
     'title' => 'Google Cloud Storage FAL Driver',
     'description' => 'Google Cloud Storage FAL driver for TYPO3. Files can be stored in the GCS buckets.',
     'category' => 'be',
-    'version' => '1.0.8',
-    'state' => 'beta',
+    'version' => '1.0.9',
+    'state' => 'stable',
     'clearcacheonload' => 1,
     'author' => 'Pierre Geyer',
     'author_email' => 'pg@next-motion.de',
     'author_company' => 'next.motion',
     'constraints' => [
         'depends' => [
-            'typo3' => '10.0.0-10.9.99',
+            'typo3' => '10.0.0-11.9.99',
         ],
     ],
 ];
